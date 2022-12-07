55d1e9ad24b364f1f84263e9e040cc46c7d6caa1
Jonas Klock <jonas.klock@hotmail.de>
2022-12-07 11:58:23 +0100

    Initial Commit

---
+++ SimpleTicTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleTicTacToe
+//
+//  Created by Jonas Klock on 07.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
